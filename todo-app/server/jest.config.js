f544deef25e01b416d4b943df93d5dc9d22f52bd
RodrigoGarciaT <rodrigo.garcia.torres04@gmail.com>
2025-06-12 21:59:08 -0600

    Update Jest configuration to handle ES modules

--- todo-app/server/jest.config.js
+++ todo-app/server/jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(nanoid)/)',
+  ],
 };
