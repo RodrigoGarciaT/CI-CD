a4a9ac5b728fc7cf73d4907998c3bbcb0b007208
RodrigoGarciaT <rodrigo.garcia.torres04@gmail.com>
2025-06-12 22:04:50 -0600

    Update Jest configuration for better ES modules support

--- todo-app/server/jest.config.mjs
+++ todo-app/server/jest.config.mjs
@@ -1,17 +1,18 @@
 const config = {
-  preset: 'ts-jest/preset/default-esm',
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   transform: {
     '^.+\\.ts$': ['ts-jest', {
       useESM: true,
-    }],
+      tsconfig: 'tsconfig.json'
+    }]
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!(nanoid)/)',
-  ],
+  extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: ['node_modules/(?!(nanoid)/)'],
+  testMatch: ['**/__tests__/**/*.test.ts']
 };
 
 export default config;
